2a43ffbd6e5b789488f8972af4e8dec5244147d9
muzman01 <supurge_man@hotmail.com>
2023-10-18 15:30:03 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-interface Color {
-  red: number;
-  green: number;
-  blue: number;
-}
+// all error hooks
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-const color: Color = {
-  red: 255,
-  green: 255,
-  blue: 255,
-};
-
-console.log(color);
-
-export default color;
+// all auth middleware hooks
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
