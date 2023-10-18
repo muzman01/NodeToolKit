9c032b224ea80985bc3845f6a20d3013a4f62d4a
muzman01 <supurge_man@hotmail.com>
2023-10-18 15:10:43 +0300

    additional files

---
+++ build /index.js
@@ -0,0 +1,7 @@
+"use strict";
+const color = {
+    red: 255,
+    green: 255,
+    blue: 255,
+};
+console.log(color);
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 255,
+  green: 255,
+  blue: 255,
+};
+
+console.log(color);
+
+export default color;
