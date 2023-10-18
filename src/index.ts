30109f66aef4728cbdc14fbbb85e31a2a836ebe8
muzman01 <supurge_man@hotmail.com>
2023-10-18 17:11:23 +0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 // all error hooks
-export * from "./errors/bad-request-error";
-export * from "./errors/custom-error";
-export * from "./errors/database-connection-error";
-export * from "./errors/not-authorized-error";
-export * from "./errors/not-found-error";
-export * from "./errors/request-validation-error";
-
+export * from "./errors/error-handler";
+export * from "./errors/result-handler";
 // all auth middleware hooks
-export * from "./middlewares/error-handler";
+
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
-export * from "./middlewares/result-handler";
