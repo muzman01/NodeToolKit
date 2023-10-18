8796af2b6624fe6c5d83c69b872e5d950e699891
muzman01 <supurge_man@hotmail.com>
2023-10-18 16:47:03 +0300

    Updates

--- src/middlewares/validate-request.ts
+++ src/middlewares/validate-request.ts
@@ -1,17 +1,29 @@
-import { Request, Response, NextFunction } from 'express';
-import { validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-export const validateRequest = (
-  req: Request,
+export const requireAuth = async (
+  req: any,
   res: Response,
   next: NextFunction
 ) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
+  if (!req.header) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
+  try {
+    let tmp = req.header("Authorization");
 
-  next();
+    const token = tmp ? tmp.slice(7, tmp.length) : "";
+    if (!token) {
+      return res.status(401).json({ message: "Invalid Authentification" });
+    }
+    jwt.verify(token, "muzman", (err: any, user: any) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid Authentification" });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
 };
