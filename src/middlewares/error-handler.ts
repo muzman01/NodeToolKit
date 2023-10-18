0b58a1de645e2692f88da59ee8dbfd1609ccfc17
muzman01 <supurge_man@hotmail.com>
2023-10-18 17:00:39 +0300

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 
-
 export const errorHandler = (
   req: Request,
   res: Response,
   err: string,
   code: number
 ) => {
-  return res.send(code).send({ errors: [{ message: err }] });
+  return res.status(code).send({ errors: [{ message: err }] });
 };
